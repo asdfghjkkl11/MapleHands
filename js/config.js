1ae8cdf3ecf1427df495243347a34f68eb21018a
asdfghjkkl11 <asdfghjkkl11@naver.com>
2023-08-29 02:40:10 +0900

    경로변경

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export let SERVER_PORT = 5678;
-export let URL_RANK = "https://maplestory.nexon.com/N23Ranking/World/Total?w=0&c=";
+export let URL_RANK = "https://maplestory.nexon.com/N23Ranking/World/Total?";
 export let URL_EQUIPMENT = "https://maplestory.nexon.com/Common/Character/Detail/{ID}/Equipment?p=";
 export let URL_CHARACTER = "https://maplestory.nexon.com/Common/Character/Detail/{ID}?p="
 export let URL_ITEM = "https://maplestory.nexon.com/Common/Resource/Item?p=";
